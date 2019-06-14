9ce8344321b09c3b2d4d44fa177dbb0f14a35841
bang <sqibang@gmail.com>
2019-06-14 10:45:09 +0800

    refactor: add css clean

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # ant-css
 
-Generate css file from antd repo
+Generate css file from antd repo for special project use only.
 
 ## How
 
 - `yarn`
-- `yarn download`
 - `yarn build`
--- index.js
+++ index.js
@@ -1,12 +1,119 @@
 const download = require("download-git-repo");
 const fs = require("fs");
+const path = require("path");
+const less = require("less");
+const LessPluginCleanCSS = require("less-plugin-clean-css");
 
-const dist = __dirname + "/ant-design";
-if (!fs.existsSync(dist)) {
-  fs.mkdirSync(dist);
+const mapping = {
+  button: "AntDesign.Button/content",
+  "date-picker": "",
+  input: "",
+  notification: "",
+  skeleton: "",
+  "time-picker": "",
+  calendar: "",
+  descriptions: "",
+  "input-number": "",
+  "page-header": "",
+  slider: "",
+  timeline: "",
+  affix: "",
+  card: "",
+  divider: "",
+  layout: "",
+  pagination: "",
+  spin: "",
+  tooltip: "",
+  alert: "",
+  carousel: "",
+  drawer: "",
+  list: "",
+  statistic: "",
+  transfer: "",
+  anchor: "",
+  cascader: "",
+  dropdown: "",
+  popover: "",
+  steps: "",
+  tree: "",
+  "auto-complete": "",
+  checkbox: "",
+  empty: "",
+  mention: "",
+  progress: "",
+  "tree-select": "",
+  avatar: "",
+  form: "",
+  mentions: "",
+  radio: "",
+  switch: "",
+  typography: "",
+  "back-top": "",
+  collapse: "",
+  grid: "",
+  menu: "",
+  rate: "",
+  table: "",
+  upload: "",
+  badge: "",
+  comment: "",
+  icon: "",
+  message: "",
+  tabs: "",
+  breadcrumb: "",
+  modal: "",
+  select: "",
+  tag: ""
+};
+
+function lessToCss(component, cssOutput) {
+  cleanCSSPlugin = new LessPluginCleanCSS();
+  console.log("[current]: %s", component);
+  const styleDist = "./ant-design/components/" + component + "/style/";
+  less
+    .render(fs.readFileSync(styleDist + "index.less", "utf-8"), {
+      plugins: [cleanCSSPlugin],
+      javascriptEnabled: true,
+      paths: ["./ant-design/components/style/", styleDist],
+      cleanCss: true
+    })
+    .then(
+      function(output) {
+        const dist = path.join("../src/", cssOutput);
+        if (fs.existsSync(dist)) {
+          fs.writeFileSync(path.join(dist, "style.css"), output.css);
+          console.log("[%s] done", component);
+        }
+      },
+      function(error) {
+        console.log("[%s] error: %s ", component, error);
+      }
+    );
 }
 
-console.log("Downloading... This may take a few minutes");
-download("ant-design/ant-design", dist, function(err) {
-  console.log(err ? "Error" : "Success");
-});
+function fetchAntDesign() {
+  const dist = __dirname + "/ant-design";
+
+  if (!fs.existsSync(dist)) {
+    fs.mkdirSync(dist);
+  }
+  console.log("Downloading ant-design... This may take a few minutes");
+  download("ant-design/ant-design", dist, function(err) {
+    if (err) {
+      console.log(
+        "Download ant-design failed. please check your network connection and try again later"
+      );
+      return;
+    }
+
+    for (const m in mapping) {
+      if (mapping.hasOwnProperty(m)) {
+        const element = mapping[m];
+        if (!element) continue;
+        lessToCss(m, element);
+      }
+    }
+  });
+}
+
+fetchAntDesign();
